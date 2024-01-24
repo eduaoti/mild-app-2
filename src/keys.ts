b92ef879f5b377c34112b061001a43708d5e9593
Eduardo <Eduardo>
2024-01-24 00:57:39 -0600

    Hola

---
+++ src/keys.ts
@@ -0,0 +1,12 @@
+import { PORT } from "./config"
+
+export default {
+
+    database: {
+        host: 'roundhouse.proxy.rlwy.net',
+        user: 'railway',
+        password: '4CeEf41EDHHAHHhb-Cgf6Bb4b4dff3A1',
+        database: 'railway'
+    }
+
+}
