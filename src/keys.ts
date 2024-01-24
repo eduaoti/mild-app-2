cf5c0c41021ade8b1d3efaf04b810ecda8614a68
Eduardo <135056458+eduaoti@users.noreply.github.com>
2024-01-24 01:36:42 -0600

    Update keys.ts

--- src/keys.ts
+++ src/keys.ts
@@ -3,9 +3,9 @@ import { PORT } from "./config"
 export default {
 
     database: {
-        host: 'roundhouse.proxy.rlwy.net',
-        user: 'railway',
-        password: '4CeEf41EDHHAHHhb-Cgf6Bb4b4dff3A1',
+        host: 'viaduct.proxy.rlwy.net',
+        user: 'root',
+        password: 'CDA53ge1aAbfHG4D4e461-GdH3E2b6C6',
         database: 'railway'
     }
 
